6a9f3bd4651d6385616af8d02d55a1c124dd25bf
Dharun Kumar  S H <freebirdprod.official@gmail.com>
2025-10-13 21:10:18 +0530

    pre-final

---
+++ app/dashboard/goals-tasks/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GoalsTasks = () => {
+  return (
+    <div>GoalsTasks</div>
+  )
+}
+
+export default GoalsTasks
---
+++ app/dashboard/memory-graph/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MemoryGraph = () => {
+  return (
+    <div>MemoryGraph</div>
+  )
+}
+
+export default MemoryGraph
