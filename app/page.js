86af7344be87392197e6c40c482f0124a1559c2a
Dharun Kumar  S H <freebirdprod.official@gmail.com>
2025-10-13 19:53:25 +0530

    j

--- app/page.js
+++ app/page.js
@@ -2,10 +2,13 @@
 export default function Home() {
   return (
    <div>
-    <header>
-      <h1>
+    <header className="p-3 shadow-lg flex items-center justify-between">
+      <h1 className="text-2xl font-bold text-pink-600">
         NeuraLife
       </h1>
+      <button className="bg-pink-600 font-semibold text-white px-4 py-2 rounded-lg cursor-pointer">
+        Get Started
+      </button>
     </header>
    </div>
   );
