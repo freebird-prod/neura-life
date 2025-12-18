e49fcd2230bc3b8a2bb22aa4365503f736fc7c16
Dharun Kumar  S H <freebirdprod.official@gmail.com>
2025-12-18 16:26:09 +0530

    logo update

--- app/layout.js
+++ app/layout.js
@@ -12,10 +12,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} antialiased`}
-        suppressHydrationWarning
       >
         <AuthProvider>
           <Toaster
